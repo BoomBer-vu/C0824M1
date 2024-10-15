30112f6e008f46dfbbed8662bcc8a7042c87016c
nguyenthanhvu10102003@gmail.com <nguyenthanhvu10102003@gmail.com>
2024-10-15 17:17:20 +0700

    add for Vu

--- ss12/array.js
+++ ss12/array.js
@@ -4,7 +4,7 @@ function add_element_to_array() {
     array[x] = document.getElementById("txtValue").value;
     alert("Element: " + array[x] + " Added at index " + x);
     x++;
-    document.getElementById("txtValue ").value = "";
+    document.getElementById("txtValue").value = "";
 }
 function display_array() {
     let e = "<hr/>";
